d404ff02d7f65872fb59978634ae0a6c1f07cb19
james007-codes <joshuanaidu313@gmail.com>
2026-09-06 21:04:54 +0530

    Backend api fixed

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,5 +1,4 @@
-const API_BASE_URL = "http://localhost:5000/api";
-
+const API_BASE_URL = "https://careflow-gwxc.onrender.com/api";
 /* =========================
    USER AUTHENTICATION
 ========================= */
